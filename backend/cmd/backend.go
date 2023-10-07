0823341a3487899ad5b0954424125ff3411c02c6
tomronw <thomasworthy91@gmail.com>
2023-10-07 17:54:30 +0100

    fixed backend scrapers not launching

--- backend/cmd/backend.go
+++ backend/cmd/backend.go
@@ -3,6 +3,10 @@ package main
 import (
 	"johgo-search-engine/api"
 	healthcheck "johgo-search-engine/internal/core/healthCheck"
+	"johgo-search-engine/internal/scrapers/miscellaneous"
+	"johgo-search-engine/internal/scrapers/shopify"
+	"johgo-search-engine/internal/scrapers/supermarkets"
+	"johgo-search-engine/internal/scrapers/woocommerce"
 	"sync"
 )
 
@@ -12,10 +16,10 @@ func main() {
 	// go build backend.go
 	// launch all scrapers
 	wgMain.Add(5)
-	//go miscellaneous.ScrapeMiscellaneous()
-	//go shopify.ScrapeShopifySites()
-	//go woocommerce.ScrapeWooCommerce()
-	//go supermarkets.ScrapeSupermarkets()
+	go miscellaneous.ScrapeMiscellaneous()
+	go shopify.ScrapeShopifySites()
+	go woocommerce.ScrapeWooCommerce()
+	go supermarkets.ScrapeSupermarkets()
 
 	// launch health check
 	go healthcheck.SpawnHealthCheck()
